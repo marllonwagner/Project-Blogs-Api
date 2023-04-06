d8dff9e7d1bdc46d05010e8166bd2a4419e28756
marllonwagner <marllonwagner91@gmail.com>
2023-04-06 16:37:21 -0300

    Users model create

---
+++ src/models/Users.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      displayName: DataTypes.STRING,
+      email:DataTypes.STRING,
+      password:DataTypes.INTEGER,
+      image:DataTypes.STRING,
+    },
+    { 
+      timestamps: false,
+      tableName: 'Users',
+      underscored: true,
+    },
+  );
+
+  User.associate = (models) => {
+    User.hasMany(models.blogPosts, { foreignKey: 'id', as: '' });
+  };
+
+  return User;
+};
